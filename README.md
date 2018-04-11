c5455841c4b706a0a56464850c0d58904971dc0d
Joel Pitt <joel@joelpitt.com>
2018-04-11 15:45:31 +1200

    Add interactive webcam script used for testing the individual pieces

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# face-services
+# Face Services
+
+This repository contains a number of services related to the detection, tracking, identification, and manipulation
+of faces.
+
+Initially, services will be implemented that support the following tasks:
+
+- Face localization - provides bounding boxes where faces are detected.
+- Face landmark detection - provides a set of face keypoints based on a keypoint model.
+- Face alignment - transforms face (rotate, translates, and scales) to a template landmark layout.
+- Face identity - return a vector representing the faces identity mapped to N-dimensional manifold.
+
+There are different techniques to solve these tasks, and the goal is provide multiple implementations so that
+upstream tasks can swap implementations depending on availability, price, their impact on reputation and performance,
+or other factors.
+
+## Dependencies
+
+Install Anaconda3, then open terminal with the Anaconda environment
+
+```
+conda create --name face-services python=3.6
+conda activate face-services
+pip install -r requirements.txt
+```
+
+You also need to download the following models from dlib:
+http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2
+http://dlib.net/files/shape_predictor_5_face_landmarks.dat.bz2
+http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+
+## Webcam test
+
+Eventually all services will call each other via some RPC mechanism, but while attempting to get each part working well
+there is a `webcam_test.py` script.
+
+This will activate your webcam and overlay outputs from each stage of processing, run with `python webcam_test.py`
+from the conda `face-services` environment you created above.
+
+## Launch Services
+
+**TODO** Once individual steps work within the live webcam demo, they'll be wrapped up with grpc service definitions and
+an automatic launcher launcher.
---
+++ webcam_test.py
@@ -0,0 +1,137 @@
+import sys
+import dlib
+from skimage import io
+import numpy as np
+import cv2
+
+# TODO: script to auto-fetch and extract these to the models/ dir
+# http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2
+# http://dlib.net/files/shape_predictor_5_face_landmarks.dat.bz2
+# http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+
+# external models
+landmark68_predictor_path = "models/shape_predictor_68_face_landmarks.dat"
+landmark5_predictor_path = "models/shape_predictor_5_face_landmarks.dat"
+recognition_model_path = "models/dlib_face_recognition_resnet_model_v1.dat"
+cascade_path = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+
+# dlib models
+detector = dlib.get_frontal_face_detector()
+landmark68_predictor = dlib.shape_predictor(landmark68_predictor_path)
+landmark5_predictor = dlib.shape_predictor(landmark5_predictor_path)
+facerec = dlib.face_recognition_model_v1(recognition_model_path)
+
+# opencv models
+faceCascade = cv2.CascadeClassifier(cascade_path)
+
+
+def show_landmarks(frame, detected_landmarks):
+    landmarks = np.matrix([[p.x, p.y] for p in detected_landmarks])
+    for idx, point in enumerate(landmarks):
+        pos = (point[0, 0], point[0, 1])
+
+        # annotate the positions
+        cv2.putText(frame, str(idx), pos,
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=0.4,
+                    color=(0, 0, 255))
+
+        # draw points on the landmark positions
+        cv2.circle(frame, pos, 3, color=(0, 255, 255))
+
+
+def detect_from_webcam():
+    cap = cv2.VideoCapture(0)
+    do_loop = True
+
+    landmark_predictors = [landmark5_predictor, landmark68_predictor]
+    landmark_idx = 0
+
+    num_face_detectors = 2
+    face_idx = 0
+
+    last_identity = np.zeros((128,))
+
+    while(do_loop):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        if face_idx == 0:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.05,
+                minNeighbors=5,
+                minSize=(100, 100),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+
+            dets = []
+            # Convert to array of dlib rectangles
+            for (x, y, w, h) in faces:
+                dets.append(dlib.rectangle(x, y, x+w, y+h))
+
+        else:
+            dets = detector(img, 1)
+
+
+        print("Number of faces detected: {}".format(len(dets)))
+        for i, d in enumerate(dets):
+            #print("Detection {}, score: {}, face_type:{}".format(
+            #    d, scores[i], idx[i]))
+            print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+                i, d.left(), d.top(), d.right(), d.bottom()))
+            cv2.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), (0, 255, 0), 2)
+
+            landmark_predictor = landmark_predictors[landmark_idx]
+
+            detection_object = landmark_predictor(img, d)
+            detected_landmarks = detection_object.parts()
+            show_landmarks(frame, detected_landmarks)
+
+            face_descriptor = facerec.compute_face_descriptor(img, detection_object)
+
+            # TODO: this currently is just comparing to the last frame. Won't handle multiple faces.
+            # To handle multiple faces we need to compare distance to all previous identities, or
+            # track the bbox movement
+            new_identity = np.matrix(face_descriptor)
+            print("Distance to last identity", np.linalg.norm(last_identity - new_identity))
+            last_identity = new_identity
+
+        cv2.imshow("Faces found", frame)
+        key = cv2.waitKey(1)
+        if key != -1:
+            if key == ord('q'):
+                do_loop = False
+            elif key == ord('l'):
+                landmark_idx += 1
+                landmark_idx %= len(landmark_predictors)
+            elif key == ord('d'):
+                face_idx += 1
+                face_idx %= num_face_detectors
+
+
+def process_and_show_detected_faces():
+    # Old dlib UI doing batch processing.
+    win = dlib.image_window()
+
+    for f in sys.argv[1:]:
+        print("Processing file: {}".format(f))
+        img = io.imread(f)
+        # The 1 in the second argument indicates that we should upsample the image
+        # 1 time.  This will make everything bigger and allow us to detect more
+        # faces.
+        dets = detector(img, 1)
+        print("Number of faces detected: {}".format(len(dets)))
+        for i, d in enumerate(dets):
+            print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+                i, d.left(), d.top(), d.right(), d.bottom()))
+
+        win.clear_overlay()
+        win.set_image(img)
+        win.add_overlay(dets)
+        dlib.hit_enter_to_continue()
+
+if __name__ == "__main__":
+    detect_from_webcam()
