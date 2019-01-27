bafe138d876583ff4bcd62825dfec231b0e38277
Joel Pitt <joel@joelpitt.com>
2019-01-28 09:33:21 +1300

    Config parameter for snetd has changed

--- run_services.py
+++ run_services.py
@@ -59,7 +59,7 @@ def main():
 def start_snetd(cwd, daemon_config_path=None):
     cmd = ["snetd"]
     if daemon_config_path is not None:
-        cmd.extend(["--config-path", str(daemon_config_path)])
+        cmd.extend(["--config", str(daemon_config_path)])
         return subprocess.Popen(cmd, cwd=str(cwd))
     return None
 
