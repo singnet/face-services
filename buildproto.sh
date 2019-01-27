c60334789d01ac92acfaef1d4e8ce02bd4731a01
Joel Pitt <joel@joelpitt.com>
2019-01-28 09:55:38 +1300

    Use the right python

--- buildproto.sh
+++ buildproto.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services/grpc/face_common.proto
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services/grpc/face_detect.proto
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services/grpc/face_landmarks.proto
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services/grpc/face_recognition.proto
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services/grpc/face_alignment.proto
+python3.6 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services/grpc/face_common.proto
+python3.6 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services/grpc/face_detect.proto
+python3.6 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services/grpc/face_landmarks.proto
+python3.6 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services/grpc/face_recognition.proto
+python3.6 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services/grpc/face_alignment.proto
