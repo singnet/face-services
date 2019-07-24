aee98448bfd4ba54c72540a8bd1b54b122af7de2
Joel Pitt <joel@joelpitt.com>
2019-07-24 15:14:11 +1200

    Fix some of the grpc clients so they are functional locally

--- clients/face_detect_client.py
+++ clients/face_detect_client.py
@@ -1,15 +1,15 @@
 import grpc
 import sys
+import os
 import argparse
 from getpass import getpass
 
 import services.grpc.face_detect_pb2
 import services.grpc.face_detect_pb2_grpc
 from services.grpc.face_common_pb2 import ImageRGB
+from services import registry
 
-from snet_sdk import Snet
-
-def make_request(filenamem, request_cls=None):
+def make_request(filename, request_cls=None):
     if request_cls is None:
         request_cls = ImageRGB
     with open(filename, 'rb') as infile:
@@ -20,10 +20,11 @@ def find_faces(stub, image_fn):
     return stub.FindFace(make_request(image_fn))
 
 def run_snet(image_fn, private_key):
+    from snet_sdk import Snet
     snet = Snet(private_key=private_key)
     client = snet.client("snet", "face-detect")
     stub = client.grpc.face_detect_pb2_grpc.FaceDetectStub(client.grpc_channel)
-    request = make_request(image_fn, client.grpc.translate_pb2.Request)
+    request = make_request(image_fn, client.grpc.face_detect_pb2.Request)
     return stub.FindFace(request)
     
 
@@ -39,8 +40,9 @@ def main():
     parser = argparse.ArgumentParser(prog=script_name)
     subcommand = parser.add_subparsers(dest='subcommand')
 
-    default_endpoint = 'http://localhost:50051'
-    parser.add_argument("--endpoint", help="jsonrpc server to connect to", default=default_endpoint,
+    server_name = "_".join(os.path.splitext(os.path.basename(script_name))[0].split('_')[:2]) + "_server"
+    default_endpoint = "127.0.0.1:{}".format(registry[server_name]['grpc'])
+    parser.add_argument("--endpoint", help="grpc server to connect to", default=default_endpoint,
                         type=str, required=False)
     parser.add_argument("--snet", help="call service on SingularityNet - requires configured snet CLI",
                         action='store_true')
@@ -65,8 +67,9 @@ def main():
         print("Rendering bounding box and saving to {}".format(args.out_image))
         import cv2
         image = cv2.imread(args.image)
-        for d in response['faces']:
-            cv2.rectangle(image, (d['x'], d['y']), (d['x'] + d['w'], d['y'] + d['h']), (0, 255, 0), 2)
+        
+        for d in response.face_bbox:
+            cv2.rectangle(image, (d.x, d.y), (d.x + d.w, d.y + d.h), (0, 255, 0), 2)
         cv2.imwrite(args.out_image, image)
 
 if __name__ == '__main__':
